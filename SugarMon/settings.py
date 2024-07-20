177e25e23b46627c52d46ccbfd956d7e198c3fc2
kimdevv <kim__dev@hufs.ac.kr>
2024-07-20 15:03:27 +0900

    [feat] User 모델 생성

--- SugarMon/settings.py
+++ SugarMon/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'SugarMon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'), # DB(스키마) 이름
-        'USER': config('DB_USER'), # 유저 이름 (root)
-        'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
-        'HOST': config('DB_HOST'), # DB 엔드포인트
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('DB_NAME'), # DB(스키마) 이름
+#         'USER': config('DB_USER'), # 유저 이름 (root)
+#         'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
+#         'HOST': config('DB_HOST'), # DB 엔드포인트
+#         'PORT': 3306,
+#     }
+# }
 
 
 # Password validation
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
 
 try:
     from .local_settings import *
--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    REQUIRED_FIELDS = []
+    email = None
+    username = models.CharField(max_length=100, unique = True)
+    nickname = models.CharField(max_length=100)
+    isDoctor = models.BooleanField()
