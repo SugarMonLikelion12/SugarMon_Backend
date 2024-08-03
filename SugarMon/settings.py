00e45f57126c55ff09a7f6acc1e5ccd2484228cc
kimdevv <kim__dev@hufs.ac.kr>
2024-08-03 15:32:50 +0900

    [fix] CORS 에러

--- SugarMon/settings.py
+++ SugarMon/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -159,6 +161,13 @@ SWAGGER_SETTINGS = {
       }
    }
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000"
+    "http://127.0.0.1:8000"
+    ]
+CORS_ALLOW_CREDENTIALS = True
 
 
 try:
