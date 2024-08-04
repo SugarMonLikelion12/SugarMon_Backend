ab5886daeb44b8d1d629c9764f4de3dd0d7700e0
kimdevv <kim__dev@hufs.ac.kr>
2024-08-04 23:59:34 +0900

    asgi

--- SugarMon/asgi.py
+++ SugarMon/asgi.py
@@ -10,14 +10,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SugarMon.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SugarMon.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(), # http 요청은 기존 방식을 채용
+    "http": django_asgi_app, # http 요청은 기존 방식을 채용
     "websocket": URLRouter(chat.routing.websocket_urlpatterns)
 })
