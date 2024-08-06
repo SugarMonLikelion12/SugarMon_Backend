a9b82f47c8e5c436040ed7460328c0f6f369e71b
kimdevv <kim__dev@hufs.ac.kr>
2024-08-06 21:29:50 +0900

    [feat] 체크리스트 연속 일 가져오기

--- checkList/urls.py
+++ checkList/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from .views import ChecklistListCreateView, ChecklistDetailView, DailyChecklistView
+from .views import *
 
 urlpatterns = [
     path('items/', ChecklistListCreateView.as_view(), name='checklist-list-create'),
     path('items/<int:pk>/', ChecklistDetailView.as_view(), name='checklist-detail'),
     path('daily/', DailyChecklistView.as_view(), name='daily-checklist'),
+    path('getContinuousDay/', countContinuousChecklist.as_view())
 ]
 
